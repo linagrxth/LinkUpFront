397fd8e2f113ebe5dbb91291ab45ab7ee90a03cd
klarak04 <107683806+klarak04@users.noreply.github.com>
2023-07-02 15:46:32 +0200

    kommentare

--- src/routes/angemeldet/+page.svelte
+++ src/routes/angemeldet/+page.svelte
@@ -1,17 +1,73 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { writable } from 'svelte/store';
-	import {Avatar,} from '@skeletonlabs/skeleton';
+  import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
+  import { Avatar, Modal, modalStore } from '@skeletonlabs/skeleton';
+  import type { ModalSettings } from '@skeletonlabs/skeleton';
 
-	export let posts = writable<{ id: number; user: string; createdAt: string; content: string; isFavorite: boolean; likes: number }[]>([]);
+  export let id = 1;
+  export let user = 'Klara';
+  export let createdAt = '';
 
-    //export let data;
-	export let writing = '';
-	let id = 1;
-	export let user = 'Klara';
-	export let createdAt = '12-09-2023';
+  const initialPosts = [
+    {
+      id: id++,
+      user: user,
+      createdAt: createdAt,
+      content: 'Das ist der Start',
+      isFavorite: false,
+      likes: 5,
+    }
+  ];
 
-	export const toggleFavorite = (post: { id: number; user: string; createdAt: string; content: string; isFavorite: boolean; likes: number }) => {
+  const initialComments = [
+    {
+      id: id++,
+      user: user,
+      createdAt: createdAt,
+      content: 'Voll guter Post',
+    }
+  ];
+
+  export const posts = writable(initialPosts);
+  export const comments = writable(initialComments);
+  export let writing = '';
+  export let commentInput = '';
+
+  export const handlePost = () => {
+    if (writing.trim() !== '') {
+      const newPost = {
+        id: id++,
+        user: user,
+        createdAt: createdAt,
+        content: writing,
+        isFavorite: false,
+        likes: 0,
+      };
+      posts.update((value) => [...value, newPost]);
+      writing = '';
+    }
+  };
+
+  export const handleComment = () => {
+    if (commentInput.trim() !== '') {
+      const newComment = {
+        id: id++,
+        user: user,
+        createdAt: createdAt,
+        content: commentInput
+      };
+      comments.update((value) => [...value, newComment]);
+      commentInput = '';
+    }
+
+    comments.update((value) => [...value]);
+    };
+
+  export const generatePost = {
+    //Funktion, um mit ChatGPT einen Post zu generieren
+  }
+
+  export const toggleFavorite = (post: { id: number; user: string; createdAt: string; content: string; isFavorite: boolean; likes: number }) => {
 		post.isFavorite = !post.isFavorite;
 		if (post.isFavorite) {
 			post.likes++;
@@ -21,33 +77,78 @@
 		posts.update((value) => [...value]);
 	};
 
-	onMount(() => {});
+    onMount(() => {
+		const now = new Date();
+        const year = now.getFullYear();
+        const month = (now.getMonth() + 1).toString().padStart(2, '0');
+        const day = now.getDate().toString().padStart(2, '0');
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        createdAt = `${day}.${month}.${year} ${hours}:${minutes}`;});
 
-	export const handlePost = () => {
-		if (writing.trim() !== '') {
-			const newPost = {
-				id: id++,
-				user: user,
-				createdAt: createdAt,
-				content: writing,
-				isFavorite: false,
-				likes: 0,
-			};
-			posts.update((value) => [...value, newPost]);
-			writing = '';
-		}
-	};
+        export let showModal = false;
+        let dialog: HTMLDialogElement;
+
+        // Funktion zum Öffnen des Modals
+        export const openModal = () => {
+            showModal = true;
+        };
+
+        // Funktion zum Schließen des Modals
+        export const closeModal = () => {
+            showModal = false;
+        };
+
+        // Dialog-Element aktualisieren, wenn showModal geändert wird
+        $: {
+            if (dialog && showModal) dialog.showModal();
+        };
+
+
 </script>
 
-
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<dialog
+    bind:this={dialog}
+    on:close={() => (showModal = false)}
+    on:click|self={() => dialog.close()}
+    class="modal">
+
+<div class="modal-body">
+  <!--Anzeige-->
+      <div class="card p-4 max-h-[480px] overflow-auto space-y-4">
+      {#each $comments.slice().reverse() as comment (comment.id)}
+      <div class="flex items-center">
+        <Avatar initials={user} background="bg-primary-500" width="w-9" class="mr-4"/>
+        <div class = "inhaltComments" style="margin-left: 1vh; width: 80vh;">&nbsp;{comment.content}<br></div>      
+      </div>
+    {/each}	
+ </div>
+<div class="modal-footer">
+<!--Kommentareingabe-->
+<div class="card p-4 max-h-[480px]">
+  <form>
+		<textarea bind:value={commentInput} class="textarea" rows="1" style="height:5vh;" placeholder="Gib deinen Kommentar ein" />
+		<button type="button" class="btn variant-ghost-primary self-end" on:click={handleComment}>Kommentieren</button>
+	</form>
+</div>
+
+
+
+</dialog>
 
 <div class = "con" style="display: flex; flex-direction: row;">
 <br>
 	<form class="card p-4 flex flex-col gap-3"style="width: 400px; height: 250px;border: 1px solid #b4e2ff;">
 		<p><strong>Erstelle einen Post</strong></p>
 		<textarea bind:value={writing} class="textarea" rows="4" placeholder="Dein Post..." />
-		<button type="button" class="btn variant-ghost-primary self-end" on:click={handlePost}>Posten</button>
+		
+        <div class="buttons">
+            <button type="button" class="btn variant-ghost-primary self-end" on:click={handlePost}>Posten</button>
+            <button type="button" class="btn variant-ghost-primary self-end">Post generieren</button>
+        </div>
+        
 	</form>
 
 
@@ -70,7 +171,7 @@
 				{/if}
 			</button>
 			<h3 class="counter">{post.likes}</h3>
-			<button type="button" class="btn-icon !bg-transparent">
+			<button type="button" class="btn-icon !bg-transparent" on:click={openModal}>
 				<i class="fa fa-comment-o" aria-hidden="true"></i>
 			</button>
 		</div>
@@ -113,20 +214,41 @@
 
     .textarea{
          border: 1px solid #D8D8D8;
-		border-radius: 10px;
+		     border-radius: 10px;
     }
 
     .inhalt{
         border: 1px solid #E2E8F0 ;
         background-color: #E2E8F0;
-		border-radius: 10px;
+		    border-radius: 10px;
         margin: 5px;
         height: 60px;
     }
 
-    .cardi{
-
+    .inhaltComments{
+		    border-radius: 10px;
+        margin: 5px;
+        height: 30px;
     }
 
+    .modal-content {
+        width: 500px;
+        height: 200px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .modal-footer {
+        margin-top: auto;
+    }
+
+    .input {
+        margin-bottom: 10px;
+    }
+
+    .buttons {
+        display: flex;
+    }
 
 </style>
