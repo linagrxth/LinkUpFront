bf9f96d0099da546521aeb7937ccdee68c86072c
GrothLi <linagrxth@gmx.de>
2023-07-10 09:15:25 +0200

    Backend Profil

--- src/routes/angemeldet/other-profile/+page.svelte
+++ src/routes/angemeldet/other-profile/+page.svelte
@@ -4,15 +4,12 @@
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';  
 
-	export let tabSet: number = 0;
-	export let user = 'Klara';
-	export let postcount = '1';
-	export let followercount = '5';
-	export let followedcount = '10';
-	export let bio = '~good vibes~';
-	export let isFavorite = false;
-	export let likes = writable(0);
-	export let data;
+	let posts = [];
+  let selectedPost = null;
+  let comments = [];
+  let showModal = false;
+  let commentInput;
+  let tabSet: number = 0;
 
 	export const handleTabChange = (event: CustomEvent<number>) => {
     tabSet = event.detail;
@@ -43,84 +40,8 @@ let following = [
 
   export let id = 1;
   export let createdAt = '';
-
-  const initialPosts = [
-    {
-      id: id++,
-      user: user,
-      createdAt: createdAt,
-      content: 'Das ist der Start',
-      isFavorite: false,
-      likes: 5
-    },
-	{
-		id: id++,
-		user: user,
-		createdAt: createdAt,
-		content: 'ich will nicht weinen',
-		isFavorite: true,
-		likes: 15
-	}
-  ];
-
-  const initialComments = [
-    {
-      id: id++,
-      user: user,
-      createdAt: createdAt,
-      content: 'Voll guter Post',
-    }
-  ];
-
-  export const posts = writable(initialPosts);
-  export const comments = writable(initialComments);
-  export let writing = '';
-  export let commentInput = '';
-
-  export const handlePost = () => {
-    if (writing.trim() !== '') {
-      const newPost = {
-        id: id++,
-        user: user,
-        createdAt: createdAt,
-        content: writing,
-        isFavorite: false,
-        likes: 0,
-      };
-      posts.update((value) => [...value, newPost]);
-      writing = '';
-    }
-  };
-
-  export const handleComment = () => {
-    if (commentInput.trim() !== '') {
-      const newComment = {
-        id: id++,
-        user: user,
-        createdAt: createdAt,
-        content: commentInput
-      };
-      comments.update((value) => [...value, newComment]);
-      commentInput = '';
-    }
-
-    comments.update((value) => [...value]);
-    };
-
-  export const generatePost = {
-    //Funktion, um mit ChatGPT einen Post zu generieren
-  }
-
-  export const toggleFavorite = (post: { id: number; user: string; createdAt: string; content: string; isFavorite: boolean; likes: number }) => {
-		post.isFavorite = !post.isFavorite;
-		if (post.isFavorite) {
-			post.likes++;
-		} else {
-			post.likes--;
-		}
-		posts.update((value) => [...value]);
-	};
-
+export let writing = '';
+  
     onMount(() => {
 		const now = new Date();
         const year = now.getFullYear();
@@ -130,7 +51,7 @@ let following = [
         const minutes = now.getMinutes().toString().padStart(2, '0');
         createdAt = `${day}.${month}.${year} ${hours}:${minutes}`;});
 
-        export let showModal = false;
+      
         let dialog: HTMLDialogElement;
 
         // Funktion zum Öffnen des Modals
@@ -138,42 +59,89 @@ let following = [
             showModal = true;
         };
 
-        // Funktion zum Schließen des Modals
-        export const closeModal = () => {
-            showModal = false;
-        };
-
+       
         // Dialog-Element aktualisieren, wenn showModal geändert wird
         $: {
             if (dialog && showModal) dialog.showModal();
         };
 
+		function handleKeyDown(event: { key: string; preventDefault: () => void; }) {
+          if (event.key === "Enter") {
+            event.preventDefault(); // Verhindert das Standardverhalten des Textbereichs (Zeilenumbruch)
+            handleComment();
+          };
+        };
+
+function formatiereDatum(apiDatum) {
+    const datumUhrzeit = new Date(apiDatum);
+    const tag = datumUhrzeit.getDate();
+    const monat = datumUhrzeit.getMonth() + 1; // Monate beginnen bei 0
+    const jahr = datumUhrzeit.getFullYear();
+    const stunde = datumUhrzeit.getHours();
+    const minute = datumUhrzeit.getMinutes();
+
+    return `${tag}.${monat}.${jahr} - ${stunde}:${minute < 10 ? '0' + minute : minute} Uhr`;
+  }
+
+  let user2 = null;
+
+  async function fetchUser() {
+    const response = await fetch('https://linkup-api.de/api/users/3');
+    const data = await response.json();
+    user2 = data;
+  }
+
+
+  async function fetchPosts() {
+    const response = await fetch('https://linkup-api.de/api/posts/user/3?limit=0&page=0');
+    const data = await response.json();
+    posts = data;
+  }
+
+  async function fetchComments(postId) {
+    const response = await fetch(`https://linkup-api.de/api/comments/posts/${postId}`);
+    const data = await response.json();
+    comments = data;
+  }
+  onMount(fetchPosts); // Beim Laden der Komponente alle Posts abrufen
+
+  function showComments(postId) {
+    selectedPost = postId;
+    fetchComments(postId);
+    showModal = true;
+  }
+
+  function closeModal() {
+    showModal = false;
+  }
+
+  fetchUser();
+
 </script>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-{#each data.nutzer as nutzer}
+
+<div class="Tabs">
+
+            {#if user2}
 <div class="user">
-	<Avatar initials={nutzer.username} background="bg-primary-500" />
+	<Avatar initials={user2.username} background="bg-primary-500" />
 	<div class="user-info">
-		<span>@{nutzer.username}</span>
+		<span>@{user2.username}</span>
 	</div>
 </div>
 
-	<div style="margin-top: 3vh; margin-bottom: 3vh;">{bio}</div>
+	<div style="margin-top: 3vh; margin-bottom: 3vh;">{user2.bio}</div>
 	<div class="counts">
-		<span><span class="count">{postcount}</span>Posts</span>
-  		<span><span class="count">{followercount}</span>Followers</span>
-  		<span><span class="count">{followedcount}</span>Followed</span>
+  		<span><span class="count">{user2.numberFollowers}</span>Followers</span>
+  		<span><span class="count">{user2.numberFollowing}</span>Followed</span>
 	</div>
 		<a href = "setting/profilsetting">
 	<button type="button" class="btn btn-sm variant-ghost-primary self-end">Profil bearbeiten</button>
 	</a>
 	<br><br>
-	{/each}
-<div class="Tabs">
-
-
+{/if}
     
 <TabGroup justify="justify-center" padding="px-20 py-3" on:change={handleTabChange}>
 	<Tab bind:group={tabSet} name="tab1" value={0}>Posts</Tab>
@@ -182,59 +150,74 @@ let following = [
 	<!-- Tab Panels --->
 	<svelte:fragment slot="panel">
 		{#if tabSet == 0}	
+        <div class="centered-content">
+<div class = "con" style="display: flex; flex-direction: row;">
 
-					<dialog
-				bind:this={dialog}
-				on:close={() => (showModal = false)}
-				on:click|self={() => dialog.close()}
-				class="modal">
 
-			<div class="modal-body">
-			<!--Anzeige-->
-				<div class="card p-4 max-h-[300px] overflow-auto space-y-4">
-				{#each $comments.slice().reverse() as comment (comment.id)}
-				<div class="flex items-center">
-					<Avatar initials={user} background="bg-primary-500" width="w-9" class="mr-4"/>
-					<div class = "inhaltComments" style="margin-left: 1vh; width: 80vh;">&nbsp;{comment.content}<br></div>      
-				</div>
-				{/each}	
-			</div>
-			<div class="modal-footer">
-<div class="card p-4 max-h-[480px]"style = "border: 1px solid #b4e2ff;">
-  <form>
-		<textarea bind:value={commentInput} class="textarea" rows="1" style="height:5vh;" placeholder="Gib deinen Kommentar ein" />
-		<button type="button" class="btn variant-ghost-primary self-end" on:click={handleComment}>Kommentieren</button>
-	</form>
+
+    <div class=" bg-secondary-400 card p-4 max-h-[200px] overflow-auto space-y-4" style="border: 2px solid black; border-radius: 10px;">
+
+
+		{#each posts as post}
+	<div class=" bg-secondary-200 card p-4 flex flex-col gap-3" style = " margin:10px; border: 0.5px solid black; border-radius: 10px;" >
+		<div class="postheader">
+			<Avatar initials={post.user.username} background="bg-primary-500" width="w-9" class="mr-4"/>
+			<strong style="margin-right: 6vh;">@{post.user.username}</strong> 
+      <span style="font-size: 12px; ">{formatiereDatum(post.createdAt)}</span>
+      
+		</div>
+		<div class="n" style="margin-left: 3vh; border-radius: 5px;">&nbsp;{post.content}<br></div>
+        <div class = "actions">
+        <button type="button" class="btn-icon !bg-transparent">
+				<i class="fa fa-heart-o" aria-hidden="true"></i>
+			</button>
+      <h3 class="counter">{post.numberOfLikes}</h3>
+      <button type="button" class="btn-icon !bg-transparent" on:click={() => showComments(post.id)}>
+				<i class="fa fa-comment-o" aria-hidden="true"></i>
+			</button>
+      
+            </div>
+	</div>
+{/each}
 </div>
-			</dialog>
+</div>
 
-		<div class="card p-4 max-h-[480px] overflow-auto space-y-4" style = "border: 1px solid #b4e2ff;">
-			{#each $posts.slice().reverse() as post (post.id)}
-		<div class="card p-4 flex flex-col gap-3" style = "border: 1px solid #D8D8D8; margin:10px;" >
-			<div class="postheader">
-				<Avatar initials={user} background="bg-primary-500" width="w-9" class="mr-4"/>
-				<strong style="margin-right: 6vh;">{post.user}</strong> {post.createdAt}
-			</div>
-			<div class = "inhalt" style="margin-left: 3vh;">&nbsp;{post.content}<br></div>
-			<div class="actions">
-				<button type="button" class="btn-icon !bg-transparent" on:click={() => toggleFavorite(post)}>
-					{#if post.isFavorite}
-						<i class="fa fa-heart" aria-hidden="true"></i>
-					{:else}
-						<i class="fa fa-heart-o" aria-hidden="true"></i>
-					{/if}
-				</button>
-				<h3 class="counter">{post.likes}</h3>
-				<button type="button" class="btn-icon !bg-transparent" on:click={openModal}>
-					<i class="fa fa-comment-o" aria-hidden="true"></i>
-				</button>
-			</div>
-			</div>
-			{/each}
-			</div>
+
+{#if showModal}
+
+  <div class="modal"style="display: flex; align-items: center; justify-content: center;">
+    <div class="bg-secondary-600 modal-content"style="border: 1px solid black; width: 50%;" >
+      <span class="close" style= "color:black;" on:click={closeModal}>&times;</span>
+      <h3>Kommentare</h3>
+      <ul>
+      <div class="card p-4 max-h-[400px] overflow-auto space-y-4" style="border: 1px solid black;">
+        {#each comments as comment}
+        <div class="flex items-center">
+          <Avatar initials={comment.user.username} background="bg-primary-500" width="w-9" class="mr-4" />
+          <li>{comment.comment}</li>
+        
+
+
+          </div>
+        {/each}
+   
+    </div>
+    <br>
+     <textarea bind:value={commentInput} class="textarea" rows="1" style="height:5vh;" placeholder=" Gib deinen Kommentar ein"/>
+     <button type="button" class="btn variant-filled">Kommentieren</button>
+    
+      </ul>
+
+    </div>
+  </div>
+
+{/if}
+
+</div>
+				
 
 		{:else if tabSet == 1}
-			<div class="centered-content">
+			<div class="centered-content" style = "width:400px;">
 				<div class="card p-4" style="width: 50vh;">
 						<ul class="list">
 							{#each follower as ben}
@@ -333,5 +316,107 @@ let following = [
     .con strong{
         font-size: 25px;
     }
+
+	.modal {
+  		border-radius: 10px;
+  		border: 1px solid black;
+	}
+  .modal {
+    display: block;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+
+  .modal-content {
+    margin: 20% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+    border-radius: 10px;
+  }
+
+  .close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+  }
+
+  .close:hover,
+  .close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .actions {
+		display: flex;
+		text-align: left;
+	}
+
+ 
+	.postheader {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 6px;
+}
+
+.postheader span {
+  font-size: 12px;
+  margin-left: auto;
+}
+
+
+	.actions {
+		display: flex;
+		text-align: left;
+	}
+
+    .con {
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+}
+
+	.counter {
+		margin-top: 6px;
+	}
+
+    .card {
+		margin-bottom: 20px; 
+        margin: 20px;
+        flex: 1;
+	}
+
+	
+
+        .inhaltComments{
+		    border-radius: 10px;
+        margin: 5px;
+        height: 30px;
+    }
+
+
+
+    .modal-footer {
+      margin-top: auto;
+	  }
+
+    .textarea{
+      border: 1px solid #D8D8D8;
+		  border-radius: 10px;
+    }
+
+    .modal {
+  border-radius: 10px;
+  border: 1px solid black;
+}
 
 </style>
