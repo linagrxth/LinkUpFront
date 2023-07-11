c77206a7e4c231b4c1781faf400a9c7f50b3e5ab
GrothLi <linagrxth@gmx.de>
2023-07-11 11:21:36 +0200

    testi

--- src/routes/angemeldet/test2/+page.svelte
+++ src/routes/angemeldet/test2/+page.svelte
@@ -2,43 +2,27 @@
   import { onMount } from 'svelte';
   import { Avatar, Modal, modalStore } from '@skeletonlabs/skeleton';
   import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
+  import { popup } from '@skeletonlabs/skeleton';
+  import { createEventDispatcher } from 'svelte';
+
 
   let posts = [];
   let selectedPostId = null;
   let comments = [];
-  let showModal = false; // Added variable to control modal visibility
-
+  let commentInput = '';
   export let writing = '';
+  let tabSet = 0;
+  let showModal = false;
+  const dispatch = createEventDispatcher();
 
-  const createPost = async () => {
-    try {
-      const response = await fetch('https://linkup-api.de/api/posts', {
-        mode: 'cors',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        body: JSON.stringify({
-          content: 'Hey :)!'
-        })
-      });
 
-      if (response.ok) {
-        console.log(response.status);
-        const responseBody = await response.json();
-        console.log(responseBody);
-      } else {
-        throw new Error('Error creating post');
-      }
-    } catch (error) {
-      console.error(error);
-    }
+  const handleLogin = async () => {
+    // ...
   };
 
   const getPosts = async () => {
     try {
-      const response = await fetch('https://linkup-api.de/api/posts', {
+      const response = await fetch('https://linkup-api.de/api/posts?limit=67', {
         mode: 'cors',
         method: 'GET',
         headers: {
@@ -50,10 +34,11 @@
       if (response.ok) {
         posts = await response.json();
       } else {
-        throw new Error('Error retrieving posts');
+        throw new Error('Fehler beim Abrufen der Posts');
       }
     } catch (error) {
       console.error(error);
+      comments = [];
     }
   };
 
@@ -70,9 +55,30 @@
 
       if (response.ok) {
         comments = await response.json();
-        showModal = true;
       } else {
-        throw new Error('Error retrieving comments');
+        throw new Error('Fehler beim Abrufen der Kommentare');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const likePost = async (postId) => {
+    try {
+      const response = await fetch(`https://linkup-api.de/api/likes/${postId}`, {
+        mode: 'cors',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        console.log('Post wurde geliked');
+        console.log(response.status);
+      } else {
+        throw new Error('Fehler beim Liken des Posts');
       }
     } catch (error) {
       console.error(error);
@@ -82,15 +88,33 @@
   const handlePostClick = async (postId) => {
     selectedPostId = postId;
     await getPostComments(postId);
+    showModal = true;
   };
 
   onMount(async () => {
     try {
+      await handleLogin();
       await getPosts();
     } catch (error) {
       console.error(error);
     }
   });
+
+  
+  function openModal() {
+    showModal = true;
+  }
+
+  function closeModal() {
+    showModal = false;
+  }
+
+  function handleClick() {
+    openModal();
+    dispatch('buttonClick');
+  }
+
+ 
 
   function formatiereDatum(apiDatum) {
     const datumUhrzeit = new Date(apiDatum);
@@ -102,14 +126,15 @@
 
     return `${tag}.${monat}.${jahr} - ${stunde}:${minute < 10 ? '0' + minute : minute} Uhr`;
   }
-</script>
 
+
+</script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<div class="con">
+<div class="con" style="display: flex; flex-direction: row;">
   <div class="bg-secondary-400 card p-4 max-h-[440px] overflow-auto space-y-4" style="border: 2px solid black; border-radius: 10px;">
     {#each posts as post}
-      <div class="bg-secondary-200 card p-4 flex flex-col gap-3" style="margin: 10px; border: 0.5px solid black; border-radius: 10px;">
+      <div class="bg-secondary-200 card p-4 flex flex-col gap-3" style="margin: 10px; border: 0.5px solid black; border-radius: 10px;" >
         <div class="postheader">
           <Avatar initials={post.user.username} background="bg-primary-500" width="w-9" class="mr-4" />
           <strong style="margin-right: 6vh;">@{post.user.username}</strong>
@@ -117,7 +142,7 @@
         </div>
         <div class="n" style="margin-left: 3vh; border-radius: 5px;">&nbsp;{post.content}<br></div>
         <div class="actions">
-          <button type="button" class="btn-icon !bg-transparent">
+          <button type="button" class="btn-icon !bg-transparent" on:click={() => likePost(post.id)}>
             <i class="fa fa-heart-o" aria-hidden="true"></i>
           </button>
           <h3 class="counter">{post.numberOfLikes}</h3>
@@ -129,64 +154,88 @@
     {/each}
   </div>
 </div>
+		
+<div>
+  {#each posts as post}
+    <div on:click={() => handlePostClick(post.id)}>
+      <p>Username: {post.user.username}</p>
+      <p>Post: {post.content}</p>
+    </div>
+  {/each}
+</div>
 
-<Modal bind:isOpen={showModal}>
-  <div class="modal-content">
-    <span class="close" on:click={() => showModal = false}>&times;</span>
-    <h2>Comments</h2>
-    {#each comments as comment}
-      <p>{comment.text}</p>
-    {/each}
+  <div class="bg-secondary-400 card p-4 max-h-[440px] overflow-auto space-y-4" style="border: 2px solid black; border-radius: 10px;">
+<div class="bg-secondary-200 con" style="display: flex; flex-direction: row">
+    <h2>Kommentare</h2>
+{#if selectedPostId !== null}
+  <div>
+
+    {#if comments !== null}
+      {#each comments as comment}
+        <div>
+        <Avatar initials={comment.user.username} background="bg-primary-500" width="w-9" class="mr-4" />
+          <p>Username: {comment.user.username}</p>
+          <p>Kommentar: {comment.comment}</p>
+        </div>
+      {/each}
+    {:else}
+      <p>Keine Kommentare vorhanden.</p>
+    {/if}
   </div>
-</Modal>
+{/if}
+</div>
+</div>
+
+
+<button on:click={handleClick}>Button</button>
+
+{#if showModal}
+<div class="bg-secondary-400 card p-4 max-h-[440px] overflow-auto space-y-4" style="border: 2px solid black; border-radius: 10px;">
+  <div class="bg-secondary-600 modal" style="width: 400px;">
+    <div style="display: flex; justify-content: flex-end;">
+      <button type="button" class="close-button" on:click={closeModal}>
+        <svg class="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+        </svg>
+      </button>
+    </div>
+    <h3>Kommentare</h3>
+    {#if selectedPostId !== null}
+      <div>
+        {#if comments !== null}
+          <div class="card p-4 max-h-[300px] overflow-auto space-y-4" style="border: 1px solid black;">
+            {#each comments as comment}
+              <div class="flex items-center">
+                <Avatar initials={comment.user.username} background="bg-primary-500" width="w-14" class="mr-4" />
+                <div class="inhaltComments" style="margin-left: 1vh; width: 80vh;">&nbsp;{comment.comment}<br></div>
+              </div>
+            {/each}
+          </div>
+          {:else}
+          <p>Keine Kommentare vorhanden.</p>
+        {/if}
+      </div>
+    {/if}
+    <textarea bind:value={commentInput} class="textarea" rows="1" style="height:5vh;" placeholder="Gib deinen Kommentar ein"></textarea>
+    <button type="button" class="btn variant-ghost-surface">Kommentieren</button>
+  </div>
+</div>
+{/if}
+
+
+
+
 
 <style>
-  .con {
-    display: flex;
-    flex-direction: row;
-  }
-
-  .card {
-    border: 2px solid black;
-    border-radius: 10px;
-  }
-
-  .post {
-    margin: 10px;
-    border: 0.5px solid black;
-    border-radius: 10px;
-  }
-
-  .postheader {
-    display: flex;
-    align-items: center;
-    margin-right: 6vh;
-  }
-
-  .n {
-    margin-left: 3vh;
-    border-radius: 5px;
-  }
-
-  .actions {
-    display: flex;
-    align-items: center;
-  }
-
-  .btn-icon {
-    background-color: transparent;
-  }
-
-  .modal {
-    display: block;
+   .modal {
     position: fixed;
-    z-index: 1;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    overflow: auto;
-    background-color: rgba(0, 0, 0, 0.4);
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 20px;
+    border: 1px solid black;
+    border-radius: 4px;
+    z-index: 9999;
   }
 
   .modal-content {
@@ -212,34 +261,69 @@
   }
 
   .actions {
-    display: flex;
-    text-align: left;
-  }
+		display: flex;
+		text-align: left;
+	}
 
-  .postheader {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-left: 6px;
-  }
+ 
+	.postheader {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 6px;
+}
 
-  .postheader span {
-    font-size: 12px;
-    margin-left: auto;
-  }
+.postheader span {
+  font-size: 12px;
+  margin-left: auto;
+}
 
-  .actions {
-    display: flex;
-    text-align: left;
-  }
 
-  .con {
-    display: flex;
-    flex-direction: row;
-    flex: 1;
-  }
+	.actions {
+		display: flex;
+		text-align: left;
+	}
 
-  .counter {
-   
-  }
+    .con {
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+}
+
+	.counter {
+		margin-top: 6px;
+	}
+
+    .card {
+		margin-bottom: 20px; 
+        margin: 20px;
+        flex: 1;
+	}
+
+	
+
+        .inhaltComments{
+		    border-radius: 10px;
+        margin: 5px;
+        height: 30px;
+    }
+
+
+
+    .modal-footer {
+      margin-top: auto;
+	  }
+
+    .textarea{
+      border: 1px solid #D8D8D8;
+		  border-radius: 10px;
+    }
+
+    .modal {
+  border-radius: 10px;
+  border: 1px solid black;
+}
+
+
+
 </style>
